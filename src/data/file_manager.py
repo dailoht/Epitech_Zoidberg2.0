1bc3cd1af0875530260c0a7819d516fb2e0fd049
Pierre-Alexandre Bolteau <pierre-alexandre.bolteau@epitech.eu>
2023-02-28 23:44:04 +0100

    metadata analysis

--- src/data/file_manager.py
+++ src/data/file_manager.py
@@ -11,6 +11,7 @@ SRC_ROOT_DIR = PROJECT_ROOT_DIR / "src"
 SOURCE_DIR = DATA_ROOT_DIR / "raw" / "chest_Xray"
 DEST_DIR = DATA_ROOT_DIR / "interim" / "full"
 
+
 # Useful project information
 class ProjectInfo():
     """_summary_
@@ -23,13 +24,14 @@ class ProjectInfo():
         self.model_dir = MODEL_ROOT_DIR
         self.srcipts_dir = SRC_ROOT_DIR
 
+
 # Functions for fetching data
 def create_folders(exist_ok=True):
     """_summary_
     Function that creates class folder in full dataset.
-    
+
     Args:
-        exist_ok (bool, optional). Default is True
+        exist_ok (bool, optional). Defaults to True.
 
     Returns:
         normal_dir: Path
@@ -42,14 +44,14 @@ def create_folders(exist_ok=True):
     virus_dir.mkdir(parents=True, exist_ok=exist_ok)
     bacteria_dir = DEST_DIR / 'bacteria'
     bacteria_dir.mkdir(parents=True, exist_ok=exist_ok)
-    
+
     return normal_dir, virus_dir, bacteria_dir
 
 
 def dest_file_name(directory, index):
     """_summary_
     Function that generates filename for images inside full dataset.
-    
+
     Args:
         directory (Path)
         index (int)
@@ -60,32 +62,70 @@ def dest_file_name(directory, index):
     return directory / "{}_img_{:04d}.jpeg".format(directory.name, index)
 
 
+def check_full_dataset(print_result=False):
+    """_summary_
+    Function that check if the full folder is the same as
+    the chest_Xray folder.
+    Args:
+        print_result (bool, optional). Defaults to False.
+
+    Returns:
+        bool
+    """
+    full = {}
+    for folder in DEST_DIR.glob("*"):
+        class_img = folder.name
+        nb_img = len(list(folder.glob("*.jpeg")))
+        full[class_img] = nb_img
+        if print_result:
+            print("full dataset : class {} => nb images = {}".format(
+                class_img, nb_img
+                ))
+
+    nb_normal = 0
+    nb_virus = 0
+    nb_bacteria = 0
+    for file in list(SOURCE_DIR.rglob("*.jpeg")):
+        if file.parents[0].name == "NORMAL":
+            nb_normal += 1
+        if "virus" in file.name.lower():
+            nb_virus += 1
+        if "bacteria" in file.name.lower():
+            nb_bacteria += 1
+    source = {"bacteria": nb_bacteria, "normal": nb_normal, "virus": nb_virus}
+    if print_result:
+        print('-'*40)
+        for key, value in source.items():
+            print("raw data     : class {} => nb images = {}".format(
+                key, value
+                ))
+    return full == source
+
+
 def fetch_full_dataset():
     """_summary_
     Final function for fetching data from chest_Xray folder to full
     folder.
     """
-    all_files = list(SOURCE_DIR.rglob("*.jpeg"))
-    nb_files = len(all_files)
-    
-    if len(list(DEST_DIR.rglob("*.jpeg"))) == nb_files:
-        print("Clean dataset already created")
+    if check_full_dataset(print_result=False):
+        print("Full dataset already created")
     else:
         normal_dir, virus_dir, bacteria_dir = create_folders()
-        
+
         idx_normal = 1
         idx_virus = 1
         idx_bacteria = 1
-        for file in all_files:
-                if file.parents[0].name == "NORMAL":
-                    shutil.copy2(file, dest_file_name(normal_dir, idx_normal))
-                    idx_normal += 1           
-                if "virus" in file.name.lower():
-                    shutil.copy2(file, dest_file_name(virus_dir, idx_virus))
-                    idx_virus += 1               
-                if "bacteria" in file.name.lower():
-                    shutil.copy2(file, dest_file_name(bacteria_dir, idx_bacteria))
-                    idx_bacteria += 1
+        for file in SOURCE_DIR.rglob("*.jpeg"):
+            if file.parents[0].name == "NORMAL":
+                shutil.copy2(file, dest_file_name(normal_dir, idx_normal))
+                idx_normal += 1
+            if "virus" in file.name.lower():
+                shutil.copy2(file, dest_file_name(virus_dir, idx_virus))
+                idx_virus += 1
+            if "bacteria" in file.name.lower():
+                shutil.copy2(file, dest_file_name(bacteria_dir, idx_bacteria))
+                idx_bacteria += 1
+
 
 if __name__ == "__main__":
-    fetch_full_dataset()
+    check_full_dataset()
--- src/visualization/plot_lib.py
+++ src/visualization/plot_lib.py
@@ -1,4 +1,5 @@
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 
 def default_viz(figsize=(7, 5), dpi=100):
@@ -7,3 +8,18 @@ def default_viz(figsize=(7, 5), dpi=100):
     mpl.rc('xtick', labelsize=10)
     mpl.rc('ytick', labelsize=10)
     mpl.rc('image', cmap='hsv')
+
+
+def plot_image(image):
+    plt.imshow(image, cmap="gray")
+    plt.axis("off")
+
+
+def plot_images(instances, figsize=(12,8)):
+    for label, images in instances.items():
+        for image in images:
+            plt.figure(figsize=figsize)
+            idx_subplot = list(instances.keys()).index(label) * 3 + images.index(image) + 1
+            plt.subplot(len(images), 3,idx_subplot)
+            plt.title(label)
+            plot_image(image)
